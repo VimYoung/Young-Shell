2b43a98f6095b5411dce3ab8f6ee88f9ba57662c
VimYoung <bhaumik2386@gmail.com>
2025-05-12 12:18:23 +0530

    Menu widget outline set

--- young-shell/src/main.rs
+++ young-shell/src/main.rs
@@ -3,39 +3,40 @@ use std::{env, error::Error};
 use spell::{
     cast_spell, get_spell_ingredients,
     layer_properties::{LayerAnchor, LayerType},
-    slint_adapter::{SlintLayerShell, SpellWinAdapter},
-    wayland_adapter::SpellWin,
+    slint_adapter::{SpellLayerShell, SpellWinAdapter},
+    wayland_adapter::{SpellWin, WindowConf},
 };
 
 slint::include_modules!();
 fn main() -> Result<(), Box<dyn Error>> {
     // env::set_var("WAYLAND_DEBUG", "1");
+    // env::set_var("RUST_BACKTRACE", "1");
     // Dimentions for the widget size
-    let width: u32 = 400; //1366;
-    let height: u32 = 400; //768;
+    let width: u32 = 376; //1366;
+    let height: u32 = 576; //768;
     let window_adapter = SpellWinAdapter::new(width, height);
     let (mut buffer1, mut buffer2) = get_spell_ingredients(width, height);
-
-    let (waywindow, work_buffer, currently_displayed_buffer, event_queue) = SpellWin::invoke_spell(
-        "counter widget",
+    let window_conf = WindowConf::new(
         width,
         height,
-        &mut buffer1,
-        &mut buffer2,
-        LayerAnchor::BOTTOM,
+        (Some(LayerAnchor::TOP), Some(LayerAnchor::LEFT)),
+        (5, 0, 0, 10),
         LayerType::Top,
         window_adapter.clone(),
         false,
     );
 
-    let platform_setting = slint::platform::set_platform(Box::new(SlintLayerShell {
+    let (waywindow, work_buffer, currently_displayed_buffer, event_queue) =
+        SpellWin::invoke_spell("counter widget", &mut buffer1, &mut buffer2, window_conf);
+
+    let platform_setting = slint::platform::set_platform(Box::new(SpellLayerShell {
         window_adapter: window_adapter.clone(),
     }));
 
     if let Err(error) = platform_setting {
         panic!("{error}");
     }
-    let ui = AppWindow::new()?;
+    let _ui = Menu::new()?;
 
     //Slint Managing Inputs;
     // ui.on_request_increase_value({
@@ -66,3 +67,5 @@ fn main() -> Result<(), Box<dyn Error>> {
 // TODO cursor shape management needs to be done.
 // TODO Making the Background transparent doesn't bring the contents of layer below it.
 // This needs to be fixed
+// TODO there is some off by one error happeneing if either of width and height is not
+// a multiple of 4.
