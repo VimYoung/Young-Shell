13196349275a995d3d72b4890ba5cb124111429d
VimYoung <bhaumik2386@gmail.com>
2025-06-23 19:34:46 +0530

    Working with new Skia renderer

--- young-shell/src/main.rs
+++ young-shell/src/main.rs
@@ -1,42 +1,48 @@
-use std::{env, error::Error};
+use std::{cell::RefCell, env, error::Error, rc::Rc};
 
 use spell::{
     cast_spell, get_spell_ingredients,
     layer_properties::{LayerAnchor, LayerType, WindowConf},
-    slint_adapter::{SpellLayerShell, SpellWinAdapter},
+    shared_context::SharedCore,
+    skia_adapter::SpellSkiaWinAdapter,
+    slint_adapter::SpellLayerShell,
     wayland_adapter::SpellWin,
 };
 
 slint::include_modules!();
 fn main() -> Result<(), Box<dyn Error>> {
     // env::set_var("WAYLAND_DEBUG", "1");
-    // env::set_var("RUST_BACKTRACE", "1");
+    env::set_var("RUST_BACKTRACE", "1");
     // Dimentions for the widget size
     let width: u32 = 376; //1366;
     let height: u32 = 576; //768;
-    let window_adapter = SpellWinAdapter::new(width, height);
-    let work_buffer = get_spell_ingredients(width, height);
+    let core = Rc::new(RefCell::new(SharedCore::new(width, height)));
+    let window_adapter = SpellSkiaWinAdapter::new(core.clone(), width, height);
+    // let work_buffer = get_spell_ingredients(width, height);
     let window_conf = WindowConf::new(
         width,
         height,
         (Some(LayerAnchor::TOP), Some(LayerAnchor::LEFT)),
         (5, 0, 0, 10),
         LayerType::Top,
+        core,
         window_adapter.clone(),
         false,
     );
+    println!("Widnow Conf is set");
 
     let (waywindow, event_queue) = SpellWin::invoke_spell("counter-widget", window_conf);
 
     let platform_setting = slint::platform::set_platform(Box::new(SpellLayerShell {
-        window_adapter: window_adapter,
+        window_adapter,
         time_since_start: std::time::Instant::now(),
     }));
-
+    println!("platform for slint is set");
     if let Err(error) = platform_setting {
         panic!("{error}");
     }
     let _ui = Menu::new()?;
+    println!("ui's new window created");
 
     //Slint Managing Inputs;
     // ui.on_request_increase_value({
@@ -48,7 +54,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     // });
     //
     // println!("Casting the Spell");
-    cast_spell( waywindow, event_queue, work_buffer)
+    // ui.run()?;
+    // Ok(())
+    println!("spell is caseted");
+    cast_spell(waywindow, event_queue)
 }
 // TODO the animations are jerky, you know the reason but you have to find a solution.
 // TODO the cursor doesn't change from pointer to hand when clicking buttons, so the
