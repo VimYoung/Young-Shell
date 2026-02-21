b4147758255855627a40685e03ec47f9339064e5
VimYoung <bhaumik2386@gmail.com>
2026-02-21 18:07:43 +0530

    Modify: Minor changes

--- build.rs
+++ build.rs
@@ -1,9 +1,7 @@
 fn main() {
     let manifest_dir = std::path::PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").unwrap());
-    let library_paths = std::collections::HashMap::from([(
-        "sleek-ui".to_string(),
-        manifest_dir.join("ui/sleek-ui/"),
-    )]);
+    let library_paths =
+        std::collections::HashMap::from([("sleek".to_string(), manifest_dir.join("ui/sleek-ui/"))]);
     let config = slint_build::CompilerConfiguration::new()
         .with_style("cosmic-dark".into())
         .with_library_paths(library_paths);
